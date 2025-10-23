e734d97ed1c2586cc736bf1223a2ad9f6d013645
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-10-23 23:27:54 +0800

    Added ndroid apk configs

---
+++ app/app/src/main/java/app/vercel/solfps/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.solfps.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
